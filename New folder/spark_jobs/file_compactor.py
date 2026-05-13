85c99df5980a827ebe4cb7e4415be78a0a182367
Vivek Singh <vksingh24iit@gmail.com>
2026-05-13 21:17:25 +0530

    Remove unused folder

--- New folder/spark_jobs/file_compactor.py
+++
@@ -1,6 +0,0 @@
-from pyspark.sql import SparkSession
-
-spark = SparkSession.builder.appName("FileCompactor").getOrCreate()
-df = spark.read.json("/topics/cdr-events/*/*.json")
-df.write.mode("overwrite").parquet("/silver/cdr_compacted")
-print("Compaction done")
